93eb368f67c454024a5961c4fadfef21e154bbc0
Alyssa Mercado <amercado1199@gmail.com>
2026-05-27 18:37:17 -0400

    complete authentication users lab

--- server/app.py
+++ server/app.py
@@ -42,15 +42,51 @@ class ShowArticle(Resource):
         if session['page_views'] <= 3:
 
             article = Article.query.filter(Article.id == id).first()
-            article_json = ArticlesSchema.dump(article)
+            article_json = ArticlesSchema().dump(article)
 
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+
+        user = User.query.filter(User.username == username).first()
+
+        session['user_id'] = user.id
+
+        return UserSchema().dump(user), 200
+
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+
+        return {}, 204
+
+
+class CheckSession(Resource):
+
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.filter(User.id == user_id).first()
+
+            return UserSchema().dump(user), 200
+
+        return {}, 401
+    
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
